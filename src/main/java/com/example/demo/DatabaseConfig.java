215ad16117b4168d5266e5e21d72e4870293c0fa
SHLIM1103 <gusl5525@gmail.com>
2021-01-18 12:18:06 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/DatabaseConfig.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.apache.catalina.servlets.WebdavServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatabaseConfig {
+    @Bean
+    public ServletRegistrationBean h2servletRegistration() {
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebdavServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
+}
