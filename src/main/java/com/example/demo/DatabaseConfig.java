c2e47cbc23f881235a15b35e4503432aa42dafd8
SHLIM1103 <gusl5525@gmail.com>
2021-01-18 13:02:02 +0900

    Initial commit

--- src/main/java/com/example/demo/DatabaseConfig.java
+++ src/main/java/com/example/demo/DatabaseConfig.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import org.apache.catalina.servlets.WebdavServlet;
 import org.h2.server.web.WebServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
